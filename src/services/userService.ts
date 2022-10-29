1919baccb0276bc02e065a08ff29cfe3dfe073f7
Jonathan Dyallo <emeraldusk@gmail.com>
2022-10-29 18:00:41 -0300

    findByUsername, findById

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,8 +2,12 @@ import userRepository from "../repositories/user-repository";
 import { IUser } from "../models/interfaces/types";
 
 class UserService {
-  async findUserByUsername(id: string) {
+  async findUserById(id: string) {
     return userRepository.findUser(id);
+  }
+
+  async findUserByUsername(username: string) {
+    return userRepository.findUserByUsername(username);
   }
 
   async createUser(user: IUser) {
