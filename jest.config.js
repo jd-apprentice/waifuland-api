ef7525654b9ee8960cef641da5b82284cef2e148
Jonathan <emeraldusk@gmail.com>
2024-08-03 01:05:40 -0300

    chore: testing statements

--- jest.config.js
+++ jest.config.js
@@ -34,10 +34,10 @@ module.exports = {
   collectCoverageFrom: ["src/**/*.ts", "!**/node_modules/**"],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: -10,
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      statements: 20,
     },
   },
   transform: {
