ca814fc5d718e088c21d2b5dfcf795244459e68c
jd-apprentice <contacto@jonathan.com.ar>
2024-09-09 02:10:35 -0300

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,48 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import sonarjs from "eslint-plugin-sonarjs";
+import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import estlingPluginSecurity from "eslint-plugin-security";
 
 export default [
 	{
-		languageOptions: { globals: globals.node },
+		ignores: [
+			'__tests__',
+			'dist',
+			'docs',
+			'lib',
+			// Files
+			'**/*.md',
+		],
+	},
+	js.configs.all,
+	estlingPluginSecurity.configs.recommended,
+	eslintConfigPrettier.rules,
+	{
+		rules: {
+			'array-callback-return': ['error'],
+			'block-scoped-var': ['error'],
+			'curly': ['error'],
+			'dot-notation': ['error'],
+			'eqeqeq': ['error'],
+			'no-implicit-coercion': ['error', { boolean: false }],
+			'no-implicit-globals': ['error'],
+			'no-loop-func': ['error'],
+			'no-return-assign': ['error'],
+			'no-template-curly-in-string': ['error'],
+			'no-unneeded-ternary': ['error'],
+			'no-unused-vars': ['error', { args: 'none' }],
+			'no-useless-computed-key': ['error'],
+			'no-useless-return': ['error'],
+			'no-var': ['error'],
+			'prefer-const': [
+				'error',
+				{
+					destructuring: 'all',
+				},
+			],
+		},
 	},
 	{
-		"plugins": {
-			sonarjs,
-			pluginJs,
-			eslintConfigPrettier,
-			estlingPluginSecurity
-		}
-	}
+		languageOptions: { globals: globals.node },
+	},
 ];
