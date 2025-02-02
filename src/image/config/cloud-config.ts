ed06a07a7cf3b42198a1b18d2249e0ce813e4375
jd-apprentice <contacto@jonathan.com.ar>
2025-02-02 00:29:20 -0300

    ci: let's not check the variables..

--- src/image/config/cloud-config.ts
+++ src/image/config/cloud-config.ts
@@ -5,18 +5,8 @@ import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 // Internal Modules
 import { Config } from "src/app/config/config";
-import { rollbar } from "src/app/config/rollbar";
 
 function setConfig() {
-
-  const errorMessage = "Missing cloudinary configuration";
-
-  if (!Config.cloudinary.name || !Config.cloudinary.key || !Config.cloudinary.secret) {
-    rollbar.error(errorMessage);
-    console.error(errorMessage);
-    process.exit(1);
-  }
-
   return cloudinary.config({
     cloud_name: Config.cloudinary.name,
     api_key: Config.cloudinary.key,
