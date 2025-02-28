77f1107782a7626997de72ba1804f93bdf37a034
jd-apprentice <contacto@jonathan.com.ar>
2025-02-27 23:51:17 -0300

    style: sonarqube fixes

--- src/image/image-repository.ts
+++ src/image/image-repository.ts
@@ -2,15 +2,15 @@
 import Image from './schema/image-schema';
 import Tag from '../tag/schema/tag-schema';
 import { hasTag } from '../common/utils/ref';
-import { IImage } from './interfaces/image-interface';
+import { ImageProp } from './interfaces/image-interface';
 
 class ImageRepository {
   /**
    * @description Creates a new Image in the database
-   * @param {IImage} image - the image to create in the database
-   * @return { Promise<IImage> } - A new image created
+   * @param { ImageProp } image - the image to create in the database
+   * @return { Promise<ImageProp> } - A new image created
    */
-  async create(image: IImage): Promise<IImage> {
+  async create(image: ImageProp): Promise<ImageProp> {
     const tagExists = await Tag.findOne({ tag_id: { $eq: image.tag } });
     const _idTag = tagExists?._id;
     return Image.create({
@@ -22,9 +22,9 @@ class ImageRepository {
   /**
    * @description Get all images from the database
    * @param tag_id - the id from the tag to retrieve
-   * @return { Promise<IImage[]> } An array of images
+   * @return { Promise<ImageProp[]> } An array of images
    */
-  async findImages(tag_id?: number): Promise<IImage[]> {
+  async findImages(tag_id?: number): Promise<ImageProp[]> {
     const images = await Image.find().populate(hasTag(tag_id));
     return images.filter(image => image.tag !== null);
   }
